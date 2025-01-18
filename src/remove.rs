ba3adf211aba5dae352e7c1c38bb674049be05c1
PressJump <40802367+PressJump@users.noreply.github.com>
2025-01-18 16:46:57 +1100

    Remove Package from Package JSON

--- src/remove.rs
+++ src/remove.rs
@@ -11,5 +11,21 @@ pub fn remove(package_name: &str, current_dir: &PathBuf) -> Result<(), Box<dyn E
         return Ok(());
     }
     std::fs::remove_dir_all(package_dir)?;
+    remove_from_package_json(package_name, current_dir)?;
+    Ok(())
+}
+
+fn remove_from_package_json(package_name: &str, current_dir: &PathBuf) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let package_json = current_dir.join("package.json");
+    let package_json_str = std::fs::read_to_string(&package_json)?;
+    let package_json_value: serde_json::Value = serde_json::from_str(&package_json_str)?;
+    let dependencies = package_json_value["dependencies"].as_object().unwrap();
+    if dependencies.contains_key(package_name) {
+        let mut package_json_value = package_json_value.clone();
+        let dependencies = package_json_value["dependencies"].as_object_mut().unwrap();
+        dependencies.remove(package_name);
+        let package_json_str = serde_json::to_string_pretty(&package_json_value)?;
+        std::fs::write(&package_json, package_json_str)?;
+    }
     Ok(())
 }
